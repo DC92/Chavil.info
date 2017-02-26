9387e79db7bfe3e680783c6e8a7f1ee50fdb659b
Dominique Cavailhez <protec@cavailhez.fr>
2017-02-26 13:15:16 +0100

    Init theme

---
+++ wp-content/themes/info/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function wpm_enqueue_styles( ) {
+	wp_enqueue_style( 'elitepress', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles' );
