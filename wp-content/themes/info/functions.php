f5dff6a34608cebabdc7529628f7689f5ca8c908
Dominique Cavailhez <protec@cavailhez.fr>
2017-03-10 21:23:25 +0100

    ?search

--- wp-content/themes/info/functions.php
+++ wp-content/themes/info/functions.php
@@ -5,3 +5,15 @@ function wpm_enqueue_styles( ) {
 }
 
 add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles' );
+
+/** 
+ * Changes the URL used for the Google Sitelink Search Box functionality in WordPress SEO (Premium) 
+ * The returned string must always include {search_term} to indicate where the search term should be used.
+ * 
+ * @returns string new searchURL
+ */
+function yoast_change_ssb_search() {
+	return 'http://www.chaville-informatique.fr/?search={search_term}';
+}
+
+add_filter('wpseo_json_ld_search_url', 'yoast_change_ssb_search' ); 
